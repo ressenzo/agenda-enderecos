8a8d7345cac8a89a92dcc3da8945b56b97147b61
Renan <renancossenzo@hotmail.com>
2022-01-13 16:07:04 -0300

    Utilizando navbar do react-bootstrap para menu.

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,35 +1,21 @@
-import { Link } from 'react-router-dom';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import './Menu.css';
 
 function Menu() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light menu">
-            <div className="container-fluid">
-                <Link className="navbar-brand" to="/">Endereços</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/">
-                                Início
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/salvos">
-                                Endereços salvos
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/favoritos">
-                                Favoritos
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <Navbar className="menu" expand="md" bg="light">
+            <Container fluid>
+                <Navbar.Brand href="/">Endereços</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/">Início</Nav.Link>
+                        <Nav.Link href="/salvos">Endereços salvos</Nav.Link>
+                        <Nav.Link href="/favoritos">Favoritos</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
 
